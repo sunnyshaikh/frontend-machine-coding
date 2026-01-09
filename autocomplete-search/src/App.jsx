52c51e52230d1604722e39e5a799003386fb6380
sunnyshaikh <gitforaltaf@gmail.com>
2026-01-10 00:43:15 +0530

    caching added

--- autocomplete-search/src/App.jsx
+++ autocomplete-search/src/App.jsx
@@ -1,9 +1,10 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import AutocompleteSearch from "./AutocompleteSearch";
 
 const App = () => {
   const [input, setInput] = useState("");
   const [recipes, setRecipes] = useState([]);
+  const cacheRef = useRef(new Map());
 
   const handleChange = useCallback(
     (e) => {
@@ -13,10 +14,14 @@ const App = () => {
   );
 
   const fetchRecipes = async () => {
+    if (cacheRef.current.has(input))
+      return setRecipes(cacheRef.current.get(input));
+
     const res = await fetch(`https://dummyjson.com/recipes/search?q=${input}`);
     const data = await res.json();
 
     setRecipes(data.recipes ?? []);
+    cacheRef.current.set(input, data.recipes ?? []);
   };
 
   useEffect(() => {
