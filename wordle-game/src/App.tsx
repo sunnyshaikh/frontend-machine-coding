5cf487c4535b93f7597673fcb0058d827df1ed53
sunnyshaikh <gitforaltaf@gmail.com>
2026-01-24 23:42:58 +0530

    ui creation

--- wordle-game/src/App.tsx
+++ wordle-game/src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const ROWS = 6;
+const WORD_LENGTH = 5;
+const WORD = "HELLO";
+
+const App = () => {
+  const [guesses, setGuesses] = useState(
+    Array.from({ length: ROWS }).fill(null),
+  );
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <div className="grid">
+        {guesses.map((guess, i) => {
+          return <Row key={i} guess={guess ?? ""} />;
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+const Row = ({ guess }) => {
+  return (
+    <div className="row">
+      {Array.from({ length: WORD_LENGTH }).map((_, i) => {
+        return <div key={i} className="box"></div>;
+      })}
+    </div>
+  );
+};
+
+export default App;
