48181f5d46b6701bb7d836e9a4ac567391b33b1b
sunnyshaikh <gitforaltaf@gmail.com>
2026-01-25 21:20:41 +0530

    immediate functionality

--- javascript/debounce.js
+++ javascript/debounce.js
@@ -1,14 +1,26 @@
-function debounce(cb, delay = 300) {
-  let timer;
+function debounce(cb, delay = 300, immediate = false) {
+  let timer = null;
+
   return function (...args) {
+    const context = this;
+    const callNow = immediate && !timer;
+
     clearTimeout(timer);
+
     timer = setTimeout(() => {
-      cb.apply(this, args);
+      timer = null; // reset after delay
+      if (!immediate) {
+        cb.apply(context, args);
+      }
     }, delay);
+
+    if (callNow) {
+      cb.apply(context, args);
+    }
   };
 }
 
-const log = debounce(() => console.log("Hello"), 300);
+const log = debounce(() => console.log("fire"), 300, true);
 
 log();
 log();
