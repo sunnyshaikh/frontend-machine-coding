9935379e28eb5962ac73b179f9b22949431d1bfd
sunnyshaikh <gitforaltaf@gmail.com>
2026-01-26 22:33:13 +0530

    reduce function version

--- javascript/objectFromString.js
+++ javascript/objectFromString.js
@@ -1,11 +1,17 @@
 function constructObject(str) {
   const [path, val] = str.split("=");
   const pathArr = path.split(".");
-  let obj = val;
-  for (let i = pathArr.length - 1; i >= 0; i--) {
-    obj = { [pathArr[i]]: obj };
-  }
-  return obj;
+
+  // version 1 - for loop
+  // let obj = val;
+  // for (let i = pathArr.length - 1; i >= 0; i--) {
+  //   obj = { [pathArr[i]]: obj };
+  // }
+
+  // return obj
+
+  // version 2 - reduce right
+  return pathArr.reduceRight((acc, curr) => ({ [curr]: acc }), val);
 }
 
 console.log(constructObject("a.b.c=10"));
