36bd8c2b46b9b192286982995a808be7d2ef1654
sunnyshaikh <gitforaltaf@gmail.com>
2026-01-26 22:52:50 +0530

    implement second highest in array

---
+++ javascript/secondHighest.js
@@ -0,0 +1,17 @@
+function secondHighest(arr) {
+  let max = -Infinity;
+  let secondMax = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      secondMax = max;
+      max = arr[i];
+    } else if (arr[i] < max && arr[i] > secondMax) {
+      secondMax = arr[i];
+    }
+  }
+
+  return secondMax === -Infinity ? null : secondMax;
+}
+
+console.log(secondHighest([1, 5, 4, 2, 3]));
